9b4893fec23dada2454c060822b8256b3c60e9da
Volodymyr Mishchenko <vmishchenko2013@gmail.com>
2026-08-07 09:17:49 +0300

    Add Render domain to ALLOWED_HOSTS

--- foxtech/settings.py
+++ foxtech/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-h75!q2u_spbct%!cgz#9!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,foxtech.onrender.com,foxtech-vtmj.onrender.com').split(',')
 
 
 # Application definition

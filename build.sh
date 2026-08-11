f50d3d6cf8fd1941f7c160244744c70cad92bb6b
Volodymyr Mishchenko <vmishchenko2013@gmail.com>
2026-08-11 20:00:11 +0300

    build.sh: remove duplicate migrate, move superuser creds to env vars

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = '${ADMIN_USER:-adminost}'
+email = '${ADMIN_EMAIL:-admin@example.com}'
+password = '${ADMIN_PASSWORD:-}'
+if password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+"
